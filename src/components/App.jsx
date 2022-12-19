09df3a734c28af4a8df851d5a784233d43525178
Sofiia Shevtsova <111647791+SofiiaShevtsova@users.noreply.github.com>
2022-12-19 17:42:17 +0200

    do all for localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,19 +11,34 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contactsList = JSON.parse(localStorage.getItem('contactsList'));
+    if (contactsList) {
+      this.setState({ contacts: contactsList });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState, snapsshot) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contactsList', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addContact = contact => {
     if (this.state.contacts.find(elem => elem.name === contact.name)) {
       alert('You have this contacts');
       return;
     }
     this.setState(prevState => {
-      return { contacts: [...prevState.contacts, contact] }
+      return { contacts: [...prevState.contacts, contact] };
     });
   };
 
   removeContact = event => {
     const idContactToRemove = event.target.attributes.id.nodeValue;
-    const arrayContacts = this.state.contacts.filter(elem => elem.id !== idContactToRemove);
+    const arrayContacts = this.state.contacts.filter(
+      elem => elem.id !== idContactToRemove
+    );
     this.setState({ contacts: arrayContacts });
   };
 
@@ -35,15 +50,15 @@ export class App extends Component {
       elem.name.toLowerCase().includes(this.state.filter)
     );
     return (
-    <div
-      style={{
-        height: '100%',
-        display: 'flex',
-        flexDirection: `column`,
-        alignItems: `center`,
-        color: '#010101',
-      }}
-    >
+      <div
+        style={{
+          height: '100%',
+          display: 'flex',
+          flexDirection: `column`,
+          alignItems: `center`,
+          color: '#010101',
+        }}
+      >
         <Section title={'Phonebook'}>
           <FormAddContact addContactOnSubmit={this.addContact} />
         </Section>
